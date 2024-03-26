8a4c1f3350c431d385c904d67f8b69260dfa0cf9
Lívia Coelho <144720278+liviacoelho-mz@users.noreply.github.com>
2024-03-25 22:00:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function addShadow(icon) {
+    icon.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)"; // Adiciona sombra ao passar o mouse
+  }
+  
+  function removeShadow(icon) {
+    icon.style.boxShadow = "none"; // Remove sombra ao retirar o mouse
+  }  
+  
